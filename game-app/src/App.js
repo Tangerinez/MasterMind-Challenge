78d4c4dbbf4048827c5391b4c8e960dd59e995f4
Gregory Tang <gtang159@gmail.com>
2020-01-24 15:51:31 -0800

    Created landing page for Mastermind Game

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -3,13 +3,39 @@ import "./App.scss";
 import Banner from "./components/Banner/Banner";
 import GameBody from "./components/GameBody/GameBody";
 
-function App() {
-  return (
-    <div className="App">
-      <Banner />
-      <GameBody />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    username: ""
+  };
+
+  handleInput = event => {
+    this.setState({ username: event.target.value });
+  };
+
+  enterGame = () => {}; // checks for is user entered in a username
+
+  render() {
+    console.log(this.state.username);
+    return (
+      <div className="App">
+        <div className="section">
+          <input
+            onInput={this.handleInput}
+            className="username-input"
+            placeholder="Username"
+          />
+          <button className="enter-game-btn">Enter Game</button>
+          <div className="video-container">
+            <video autoPlay loop muted>
+              <source src="./landing-page-video.mp4" type="video/mp4"></source>
+            </video>
+          </div>
+        </div>
+        {/* <Banner />
+      <GameBody /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
